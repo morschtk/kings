fd3c84ccf6fa2164a904bd5f501dd9d7df62640f
Tyler Morsch <morschtk@gmail.com>
2020-12-10 22:14:53 -0500

    return drawn card

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const app = require('express')();
-var cors = require('cors');
+const cors = require('cors');
 app.use(cors());
 const http = require('http').createServer(app);
 const io = require('socket.io')(http, {
@@ -10,16 +10,51 @@ const io = require('socket.io')(http, {
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
+const { shuffle } = require('./utils/helpers');
+// import { shuffle } from './utils/helpers';
+
+let currPlayer = 0;
 const players = [];
+const cards = [{
+  url: '',
+  number: 1
+},{
+  url: '',
+  number: 2
+},{
+  url: '',
+  number: 3
+},{
+  url: '',
+  number: 4
+},{
+  url: '',
+  number: 5
+},{
+  url: '',
+  number: 6
+},{
+  url: '',
+  number: 7
+}];
+
+const shuffledCards = shuffle(cards);
 
 app.get('/', (req, res) => {
   res.send('<h1>Hey Socket.io</h1>');
 });
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
+  let user = socket.handshake.query.user;
+  console.log(`a user connected ${user}`);
   socket.on('disconnect', () => {
     console.log('user disconnected');
+  });
+
+  // On Draw we grab the next card and send it back to the FE
+  socket.on('draw', (msg) => {
+    const currentCard = shuffledCards.pop();
+    socket.emit('currentCard', currentCard);
   });
 });
 
@@ -32,7 +67,9 @@ app.post('/api/add-to-game', (req, res) => {
   console.log('added!');
   const user = req.body.user;
   players.push(user);
-  res.json(`${user} added`);
+  res.json({
+    index: players.length
+  });
 });
 
 http.listen(3000, () => console.log('Listening on port 3000!'));
